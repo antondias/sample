16e130c647ba751d885c12dee578b3a78c80b878
Anton Dias <antondias4@gmail.com>
2022-05-26 17:19:02 +0530

    new changes

--- metalabblender/blender.py
+++ metalabblender/blender.py
@@ -1,10 +1,10 @@
-from metalabblender import blender,tokenhandler,ldpreload
+from metalabblender import blender,tokenhandler,ldpreload,setupblender
 import subprocess
 
 class Blender:
 
 	token = None
-	filePath = None
+	blenderFilePath = None
 	outputPath = None
 	blenderVersion = None
 	fileFormat = None
@@ -13,11 +13,14 @@ class Blender:
 	endFrame = None
 	renderer = None
 	optixEnabled = None
+	gpuEnabled = None
+	cpuEnabled = None
+	animation = None
 
-	def __init__(self, filePath, outputPath, blenderVersion, fileFormat, renderEngine, startFrame, endFrame, 
-				renderer, optixEnabled, token):
+	def __init__(self, blenderFilePath, outputPath, blenderVersion, fileFormat, renderEngine, startFrame, endFrame, 
+				renderer, optixEnabled, gpuEnabled, cpuEnabled, animation, token):
 		self.token = token
-		self.filePath = filePath
+		self.blenderFilePath = blenderFilePath
 		self.outputPath = outputPath
 		self.blenderVersion = blenderVersion
 		self.fileFormat = fileFormat
@@ -26,8 +29,8 @@ class Blender:
 		self.endFrame = endFrame
 		self.renderer = renderer
 		self.optixEnabled = optixEnabled
-		Blender.gpu_setup()
-		ldpreload.preload()
+		self.gpuEnabled = gpuEnabled
+
 
 	def gpu_setup():
 		gpu = subprocess.run(["nvidia-smi", "--query-gpu=gpu_name", "--format=csv,noheader"],encoding="utf-8",stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -36,26 +39,58 @@ class Blender:
 
 		if gpu == "Tesla K80" and self.optixEnabled:
 			print("OptiX disabled because of unsupported GPU")
-			self.optixEnabled = False	
+			self.optixEnabled = False
+
+	def set_renderer():
+		if optix_enabled:
+		self.renderer = "OPTIX"		
+
+	def setup():
+		blenderRenderer = Blender()
+		blenderRenderer.gpu_setup()
+		ldpreload.preload()
+		setupblender.setup(self.blenderVersion)
+		setupblender.enable_rendering(self.gpuEnabled, self. cpuEnabled)
+		blenderRenderer.set_renderer()
+
 
 	def render(self):
 		tokenhandler.TokenHandler.test()
 		print("token = " + self.token);	
 		print("starting to process blender...")
-		if(self.startFrame == self.endFrame):
-			print("--------------------------------------------")
-			print("------ This process has been already completed------")
-			print("--------------------------------------------")
-			return
-		args = ["sudo", blender_binary, 
-				"-b", blender_file_path, 
-				"-noaudio","-E", self.renderEngine,
-				"--log-level","1",
-				"-o", self.outputPath, 
-				"-s", str(self.startFrame),
-				"-e", str(self.endFrame),
-				"-a", "--", "--cycles-device", self.renderer
-			]
+		blender_binary = './'+self.blenderVersion+"/blender"	
+		if (self.animation):
+			if start_frame == end_frame:	
+				args = ["sudo", blender_binary, 
+						"-b", blender_file_path,
+						"-P", "setgpu.py", 
+						"-noaudio","-E", self.renderEngine,
+						"--log-level","1",
+						"-o", self.outputPath,
+						"-a", "--", "--cycles-device", self.renderer
+					]
+			else:
+				args = ["sudo", blender_binary, 
+						"-b", blender_file_path,
+						"-P", "setgpu.py",
+						"-noaudio","-E", self.renderEngine,
+						"--log-level","1",
+						"-o", self.outputPath, 
+						"-s", str(self.startFrame),
+						"-e", str(self.endFrame),
+						"-a", "--", "--cycles-device", self.renderer
+					]
+		else:
+			args = ["sudo", blender_binary, 
+						"-b", blender_file_path,
+						"-P", "setgpu.py",
+						"-noaudio","-E", self.renderEngine,
+						"--log-level","1",
+						"-o", self.outputPath,
+						"-f", str(self.startFrame),
+						"--", "--cycles-device", self.renderer
+					]	
+
 		try:
 			print(' '.join(args))
 			process = subprocess.Popen(args, stdout=subprocess.PIPE)
